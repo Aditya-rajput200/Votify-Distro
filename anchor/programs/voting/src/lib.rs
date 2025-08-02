c94881f80db97f44434c2b512d3e80779c17a48f
Aditya <adityarajputsw2020@gmail.com>
2025-08-03 00:47:29 +0530

    one

---
+++ anchor/programs/voting/src/lib.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
+
+#[program]
+pub mod Voting {
+   use super::*;
+
+  
+
+}
+
